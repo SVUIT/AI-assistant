a60152a5113bfe11f072a4d812eb40b49536d903
NguyLam2704 <22520736@gm.uit.edu.vn>
2024-10-10 14:08:08 +0700

    build model using qdrant vectorstore

--- Backend/AI-chat-backend/create_chain.py
+++ Backend/AI-chat-backend/create_chain.py
@@ -1,20 +1,35 @@
 import os
-from load_data import vectordb
-from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_qdrant import QdrantVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
+
+load_dotenv()
+#create vectorstore from qdrant
+api_key_qdrant = os.environ["QDRANT_API_KEY"]
+url = os.environ["URL_QDRANT"]
+collection_name = "embedding_data"
+embedding_function = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+vectorstore = QdrantVectorStore.from_existing_collection(
+    collection_name="embedding_data",
+    embedding=embedding_function,
+    url=url,
+    api_key=api_key_qdrant,
+)
+
+#create retriever
+retriever_llm = vectorstore.as_retriever(search_type="mmr",search_kwargs={"k": 7})
+
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-
+from langchain_google_genai import ChatGoogleGenerativeAI
 google_api_key = os.environ["GEMINI_API_KEY"]
 
 
 output_parser = StrOutputParser()
 
-llm = ChatGoogleGenerativeAI(model = "gemini-1.5-flash", google_api_key = google_api_key , temperature = 0.5,
-                             top_p = 0.95, top_k = 64, max_output_tokens = 8192)
-
-# create retriever
-retriever = vectordb.as_retriever(search_type="similarity",search_kwargs={"k": 40})
+llm = ChatGoogleGenerativeAI(model = "gemini-1.5-flash", google_api_key = google_api_key , temperature = 0.2,
+                             top_p = 0.9, top_k = 1, max_output_tokens = 2000)
 
 # Create question maker
 
@@ -43,28 +58,37 @@ question_maker_prompt = ChatPromptTemplate.from_messages(
 question_chain = question_maker_prompt | llm | StrOutputParser()
 
 # build the prompt for the question and answer
-qa_system_prompt = """You are an assistant for question-answering tasks.  \
-  Use the following pieces of '{context}' to answer the question.
-  All documents in context are from courses at the University of Information Technology (UIT), Vietnam National University, Ho Chi Minh City. \
-  You can translate the question to English to find the answer.
-  You can use the course codes, Vietnamese course names, English course names in the file `courses_list.docx` to find information more easily. \
+qa_system_prompt = """You are an VietNamese-English assistant for question-answering tasks related courses of Information Technology (UIT), Vietnam National University, Ho Chi Minh City. \
+  Please use the following pieces of context{context} to generate the question at the end.
+  If you do not know the answer, please do not answer from your knowledge base and say that sorry, I do not know it. Do not recommend anything.
   Be sure to respond in a complete sentence, being comprehensive, including all relevant background information.\
   However, you are talking to a non-technical audience, so be sure to break down complicated concepts and \
   strike a friendly and converstional tone. \
+  Output result in Vietnamese.\
   If the passage is irrelevant to the answer, you may ignore it.
-  QUESTION: '{question}'
-  ANSWER:"""
+  QUESTION: {question}
+  """
 qa_prompt = ChatPromptTemplate.from_messages(
     [
+        
         ("system", qa_system_prompt),
         MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "Du lịch Việt Nam nên đi đâu?"),
+        ("ai", "Xin lỗi, Tôi không được thiết kế để trả lời câu hỏi này. Tôi chỉ có thể trả lời các câu hỏi liên quan đến công nghệ thông tin, mạng máy tính, UIT"),
+        ("human","Bạn có biết Doraemon không?"),
+        ("ai","Xin lỗi, Tôi không được thiết kế để trả lời câu hỏi này. Tôi chỉ có thể trả lời các câu hỏi liên quan đến công nghệ thông tin, mạng máy tính, UIT"),
+        ("human", "Please answer the question based the context. If you don't know the question, just say sorry, i don't know"),
+        ("ai", "Ok, I won't use my based knowledge"),
+        ("human", "Bạn có thể trả lời các câu hỏi về chủ đề gì?"),
+        ("ai", "Tôi chỉ có thể trả lời các câu hỏi dựa trên kiến thức cua các môn học của khoa mạng máy tính và truyền thông, Trường đại học Công Nghệ Thông Tin(UIT)."),
         ("human", "{question}"),
+        
     ]
 )
 
 
 retriever_chain = RunnablePassthrough.assign(
-        context=contextualized_question | retriever 
+        context=contextualized_question | retriever_llm
     )
 
 ## create  main chain that produces the final answer
--- Backend/AI-chat-backend/response.py
+++ Backend/AI-chat-backend/response.py
@@ -1,35 +1,18 @@
-from flask import Flask, request, jsonify
+chat_history = []
 from langchain_core.messages import HumanMessage
 from create_chain import rag_chain
+from get_relevant_score import get_relevant_plot, extract_answer 
 
-# Initialize Flask app
-app = Flask(__name__)
 
-# Initialize chat history
-chat_history = []
 
-@app.route('/generate', methods=['POST'])
-def generate():
-    data = request.get_json()
-    question = data.get('question', '')
-
-    if not question:
-        return jsonify({'error': 'No question provided'}), 400
-    
-    try:
-        # Invoke the RAG chain with the provided question and chat history
-        ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history})
-        
+question = "Hệ điều hành là gì"
+context = get_relevant_plot(question, top_k=3)
+score = extract_answer(question, context)
+if ( score < 0.1):
+    print("Tôi xin lỗi, tôi không có đủ thông tin để trả lời câu hỏi này")
+else:
+    ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history , "topic": "UIT, internet and information technology"})
+            
         # Update chat history with the new messages
-        chat_history.extend([HumanMessage(content=question), ai_msg])
-        
-        # Return the response from the AI
-        return jsonify({'response': ai_msg.content})
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-if __name__ == '__main__':
-    # Run the Flask app on the specified host and port
-    app.run(host='0.0.0.0', port=8000)
+    chat_history.extend([HumanMessage(content=question), ai_msg])
+    print(ai_msg.content)
